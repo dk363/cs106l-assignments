44f339a317361893986ea010fec4f0da2a98c6df
dk363 <xukuangdi465@gmail.com>
2025-09-24 21:14:59 +0800

    update

--- assignment5/user.cpp
+++ assignment5/user.cpp
@@ -1,5 +1,7 @@
 #include "user.h"
 
+#include <string>
+#include <ostream>
 /**
  * Creates a new User with the given name and no friends.
  */
@@ -15,8 +17,7 @@ User::User(const std::string& name)
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
  */
-void
-User::add_friend(const std::string& name)
+void User::add_friend(const std::string& name)
 {
   if (_size == _capacity) {
     _capacity = 2 * _capacity + 1;
@@ -34,8 +35,7 @@ User::add_friend(const std::string& name)
 /**
  * Returns the name of this User.
  */
-std::string
-User::get_name() const
+std::string User::get_name() const
 {
   return _name;
 }
@@ -43,8 +43,7 @@ User::get_name() const
 /**
  * Returns the number of friends this User has.
  */
-size_t
-User::size() const
+size_t User::size() const
 {
   return _size;
 }
@@ -63,3 +62,59 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+std::ostream& operator << (std::ostream& out, const User& user) {
+  out << "User(name=" << user._name << ", friends=[";
+  std::string* friends = user._friends;
+  for (size_t i = 0; i < user._size; ++i) {
+    out << friends[i];
+    if (i != user._size - 1) {
+      out << ", ";
+    }
+  }
+  out << "])";
+  return out;
+}
+
+User::~User() {
+  delete[] _friends;
+}
+
+// 两种拷贝方式都应该是深拷贝
+// 因为如果指向同一块内存
+// 对其中一个指针的操作会影响另一个指针
+User::User(const User& other) :
+_name(other._name), _friends(new std::string[other._capacity]),
+_size(other._size), _capacity(other._capacity) {
+  for (size_t i = 0; i < _size; ++i) {
+    _friends[i] = other._friends[i];
+  }
+}
+
+User& User::operator=(const User& other) {
+  if (this == &other) {
+    return *this;
+  }
+
+  delete[] _friends;
+  _name = other._name;
+  _size = other._size;
+  _capacity = other._capacity;
+
+  _friends = new std::string[_capacity];
+  for (size_t i = 0; i < _size; ++i) {
+    _friends[i] = other._friends[i];
+  }
+
+  return *this;
+}
+
+User &User::operator+=(User &other) {
+  this->add_friend(other._name);
+  other.add_friend(this->_name);
+  return *this;
+}
+
+bool User::operator<(const User &rhs) const {
+  return this->get_name() < rhs.get_name();
+}
+
--- assignment5/user.h
+++ assignment5/user.h
@@ -9,20 +9,38 @@
 class User
 {
 public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
-  std::string get_name() const;
-  size_t size() const;
-  void set_friend(size_t index, const std::string& name);
+    User(const std::string& name);
+    void add_friend(const std::string& name);
+    std::string get_name() const;
+    size_t size() const;
+    void set_friend(size_t index, const std::string& name);
 
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+    /**
+    * STUDENT TODO:
+    * Your custom operators and special member functions will go here!
+    */
+    /**
+    * 学生待办事项：
+    * 您的自定义运算符和特殊成员函数将放在这里！
+    */
+    friend std::ostream& operator << (std::ostream& out, const User& user);
 
+    ~User();
+
+    User(const User& other);
+
+    User& operator=(const User& other);
+
+    User(User&& user) = delete;
+
+    User& operator=(User&& user) = delete;
+
+    User& operator+=(User& other);
+
+    bool operator<(const User& rhs) const;
 private:
-  std::string _name;
-  std::string* _friends;
-  size_t _size;
-  size_t _capacity;
+    std::string _name;
+    std::string* _friends;
+    size_t _size;
+    size_t _capacity;
 };
